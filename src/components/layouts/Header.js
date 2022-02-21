0064ec90708f43180e2a88debbcb58afd0d8ba94
Amir Salehi <nik.amirsa@gmail.com>
2022-02-21 16:32:15 +0330

    remove old data

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,10 +1,7 @@
-import DaewooLogo from '../../assets/images/daewoo-logo.png';
-
 export default function Header() {
     return (
         <div className='header-main'>
-            <img src={DaewooLogo} alt="" />
-            <h1>سامانه قرعه‌کشی انتخاب</h1>
+            <h1>سامانه قرعه‌کشی</h1>
         </div>
     )
 }
