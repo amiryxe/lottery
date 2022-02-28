3844ee78d3e6607476740aaee2b54d74ffcd34c1
Amir Salehi <nik.amirsa@gmail.com>
2022-02-28 14:31:09 +0330

    install chakra ui

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import 'sahel-font/dist/font-face.css';
 import './assets/style/main.scss';
 import Main from './containers/Main';
 import MainState from './context/MainState';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function App() {
   return (
     <MainState>
-      <div className="App">
-        <Main />
+      <div className="App" theme={theme}>
+        <ChakraProvider>
+          <Main />
+        </ChakraProvider>
       </div>
     </MainState>
   );
