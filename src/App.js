97dc8cff978f619509a1b78c118ae9638a81a186
Amir Salehi <nik.amirsa@gmail.com>
2022-02-28 14:56:07 +0330

    set theme option for chakra provider

--- src/App.js
+++ src/App.js
@@ -2,23 +2,30 @@ import 'sahel-font/dist/font-face.css';
 import './assets/style/main.scss';
 import Main from './containers/Main';
 import MainState from './context/MainState';
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 
-const colors = {
-  brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+const themeOptions = {
+  colors: {
+    brand: {
+      900: '#1a365d',
+      800: '#153e75',
+      700: '#2a69ac',
+    },
   },
+  config: {
+    initialColorMode: "dark"
+  }
 }
 
-const theme = extendTheme({ colors })
+const theme = extendTheme(themeOptions)
+
+console.log(theme)
 
 function App() {
   return (
     <MainState>
-      <div className="App" theme={theme}>
-        <ChakraProvider>
+      <div className="App">
+        <ChakraProvider theme={theme}>
           <Main />
         </ChakraProvider>
       </div>
