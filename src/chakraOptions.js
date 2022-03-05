1b41e4a159d71a6854193fb10b9a27fa6702b901
Amir Salehi <nik.amirsa@gmail.com>
2022-03-05 20:39:09 +0330

    changes

--- src/chakraOptions.js
+++ src/chakraOptions.js
@@ -1,13 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const themeOptions = {
-    colors: {
-        brand: {
-            900: '#1a365d',
-            800: '#153e75',
-            700: '#2a69ac',
-        },
-    },
     config: {
         initialColorMode: "dark"
     },
