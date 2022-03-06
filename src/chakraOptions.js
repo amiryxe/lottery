c26a9e13163bbaee5b7651f7c4f9df8b6bfcf67f
Amir Salehi <nik.amirsa@gmail.com>
2022-03-06 09:33:39 +0330

    change body font

--- src/chakraOptions.js
+++ src/chakraOptions.js
@@ -4,6 +4,11 @@ const themeOptions = {
     config: {
         initialColorMode: "dark"
     },
+    fonts: {
+        body: 'Sahel, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'
+    }
 }
+
+console.log(extendTheme())
 
 export default extendTheme(themeOptions)
