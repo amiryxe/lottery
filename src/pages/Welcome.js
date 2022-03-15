bc8de77219437fcc884c30f35791aa5555f9e44c
Amir Salehi <nik.amirsa@gmail.com>
2022-03-15 12:17:54 +0330

    import drawer to welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,4 +1,13 @@
 import { Link } from 'react-router-dom';
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+} from '@chakra-ui/react'
 
 export default function Welcome() {
     return (
@@ -14,6 +23,30 @@ export default function Welcome() {
                     درباره
                 </button>
             </Link>
+
+            <Drawer
+                isOpen={isOpen}
+                placement='right'
+                onClose={onClose}
+                finalFocusRef={btnRef}
+            >
+                <DrawerOverlay />
+                <DrawerContent>
+                    <DrawerCloseButton />
+                    <DrawerHeader>Create your account</DrawerHeader>
+
+                    <DrawerBody>
+                        <Input placeholder='Type here...' />
+                    </DrawerBody>
+
+                    <DrawerFooter>
+                        <Button variant='outline' mr={3} onClick={onClose}>
+                            Cancel
+                        </Button>
+                        <Button colorScheme='blue'>Save</Button>
+                    </DrawerFooter>
+                </DrawerContent>
+            </Drawer>
         </>
     )
 }
