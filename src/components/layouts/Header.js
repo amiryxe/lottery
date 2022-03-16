8d11dcaebbc0a9ed00afd4ee365ee24cadbb8ce6
Amir Salehi <nik.amirsa@gmail.com>
2022-03-16 10:02:11 +0330

    create link

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Header() {
     return (
         <div className='header-main'>
             <h1>سامانه قرعه‌کشی</h1>
+
+            <Link to="/">
+                <button>بازگشت</button>
+            </Link>
         </div>
     )
 }
