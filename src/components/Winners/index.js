00ccf485ab1add96e81467a151c52c7e0c712d38
Amir Salehi <nik.amirsa@gmail.com>
2022-03-19 16:23:46 +0330

    changes

--- src/components/Winners/index.js
+++ src/components/Winners/index.js
@@ -3,13 +3,11 @@ import convertDigits from "../../utils/convertDigits"
 export default function Winners({ list }) {
     return (
         <div className="winners">
-            {
-                list.map(item => (
-                    <div key={item.id} className="winners__item">
-                        {convertDigits(item.value)}
-                    </div>
-                ))
-            }
+            {list.map(item => (
+                <div key={item.id} className="winners__item">
+                    {convertDigits(item.value)}
+                </div>
+            ))}
         </div>
     )
 }
