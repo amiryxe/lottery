0ecd307cd4bfdec10e44efa647e4337e77dbd2a4
Amir Salehi <nik.amirsa@gmail.com>
2022-03-19 16:32:19 +0330

    change style of back button

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -5,7 +5,11 @@ export default function Header() {
         <div className='header-main'>
             <h1>سامانه قرعه‌کشی</h1>
 
-            <Link to="/">
+            <Link to="/" style={{
+                position: 'fixed',
+                left: 5,
+                top: 5
+            }}>
                 بازگشت
             </Link>
         </div>
