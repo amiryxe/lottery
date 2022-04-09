41c71124da8b97c0128fea63022ac64d83dfbe3a
Amir Salehi <nik.amirsa@gmail.com>
2022-04-09 12:34:51 +0430

    changes

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,17 +1,23 @@
 import { Link } from 'react-router-dom';
+import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { Button } from '@chakra-ui/react';
 
 export default function Header() {
     return (
         <div className='header-main'>
             <h1>سامانه قرعه‌کشی</h1>
-
+            {/* 
             <Link to="/" style={{
                 position: 'fixed',
                 left: 5,
                 top: 5
             }}>
                 بازگشت
-            </Link>
+            </Link> */}
+
+            <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
+                Call us
+            </Button>
         </div>
     )
 }
