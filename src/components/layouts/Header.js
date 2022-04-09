22cdaaa7d03815064bf1ada73ac7328c68192e05
Amir Salehi <nik.amirsa@gmail.com>
2022-04-09 12:44:40 +0430

    install chakra ui icons and improve back button

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,23 +1,24 @@
 import { Link } from 'react-router-dom';
-import { ArrowForwardIcon } from '@chakra-ui/icons';
+import { ArrowBackIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
 
 export default function Header() {
     return (
         <div className='header-main'>
             <h1>سامانه قرعه‌کشی</h1>
-            {/* 
+
             <Link to="/" style={{
                 position: 'fixed',
-                left: 5,
-                top: 5
+                left: 16,
+                top: 16
             }}>
-                بازگشت
-            </Link> */}
-
-            <Button rightIcon={<ArrowForwardIcon />} colorScheme='teal' variant='outline'>
-                Call us
-            </Button>
+                <Button
+                    rightIcon={<ArrowBackIcon />}
+                    colorScheme='teal'
+                    variant='outline'>
+                    بازگشت
+                </Button>
+            </Link>
         </div>
     )
 }
