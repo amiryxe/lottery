c9af018ef750a4c6e9f095fbf3528d9f3166ac15
Amir Salehi <nik.amirsa@gmail.com>
2022-04-10 11:57:15 +0430

    show back button conditionally

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,24 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { Button } from '@chakra-ui/react';
 
 export default function Header() {
+    const { pathname } = useLocation();
+
     return (
         <div className='header-main'>
             <h1>سامانه قرعه‌کشی</h1>
 
-            <Link to="/" style={{
-                position: 'fixed',
-                left: 16,
-                top: 16
-            }}>
-                <Button
-                    rightIcon={<ArrowBackIcon />}
-                    colorScheme='teal'
-                    variant='outline'>
-                    بازگشت
-                </Button>
-            </Link>
+            {
+                pathname != '/' &&
+                <Link to="/" style={{
+                    position: 'fixed',
+                    left: 16,
+                    top: 16
+                }}>
+                    <Button
+                        rightIcon={<ArrowBackIcon />}
+                        colorScheme='teal'
+                        variant='outline'>
+                        بازگشت
+                    </Button>
+                </Link>
+            }
         </div>
     )
 }
