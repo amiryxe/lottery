de9b9b6e01dc32a941e6fec9b94779dc0f8c28ad
Amir Salehi <nik.amirsa@gmail.com>
2022-04-11 14:29:37 +0430

    add setting modal component to lottery page

--- src/pages/Lottery.js
+++ src/pages/Lottery.js
@@ -4,7 +4,8 @@ import Sphere from "../components/Sphere"
 import Winners from "../components/Winners"
 import confetti from "canvas-confetti"
 import convertDigits from '../utils/convertDigits'
-import { Button, useColorMode } from '@chakra-ui/react'
+import { Button, useColorMode, useDisclosure } from '@chakra-ui/react'
+import SettingModal from '../components/Setting/SettingModal'
 
 const arr = []
 
@@ -16,6 +17,8 @@ export default function Lottery() {
     const [changes, setChanges] = useState('')
 
     const { toggleColorMode } = useColorMode()
+    const { isOpen, onOpen, onClose } = useDisclosure()
+
 
     useEffect(() => {
         if (arr.length > 0) {
@@ -76,6 +79,8 @@ export default function Lottery() {
                     <path d="M2.344 15.271l2 3.46a1 1 0 001.366.365l1.396-.806c.58.457 1.221.832 1.895 1.112V21a1 1 0 001 1h4a1 1 0 001-1v-1.598a8.094 8.094 0 001.895-1.112l1.396.806c.477.275 1.091.11 1.366-.365l2-3.46a1.004 1.004 0 00-.365-1.366l-1.372-.793a7.683 7.683 0 00-.002-2.224l1.372-.793c.476-.275.641-.89.365-1.366l-2-3.46a1 1 0 00-1.366-.365l-1.396.806A8.034 8.034 0 0015 4.598V3a1 1 0 00-1-1h-4a1 1 0 00-1 1v1.598A8.094 8.094 0 007.105 5.71L5.71 4.904a.999.999 0 00-1.366.365l-2 3.46a1.004 1.004 0 00.365 1.366l1.372.793a7.683 7.683 0 000 2.224l-1.372.793c-.476.275-.641.89-.365 1.366zM12 8c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4z"></path>
                 </svg>
             </button>
+
+            <SettingModal />
         </>
     )
 }
